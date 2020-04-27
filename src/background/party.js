2aae6ccda6d5d88bcb9e494abf9d0ebbdf6581e5
Marnix <dev@primevideoparty.com>
2020-04-27 20:12:16 +0200

    Extra error handling in case of sending a message when no primevideo tabs are open

--- src/background/party.js
+++ src/background/party.js
@@ -87,7 +87,9 @@ function generatePartyId(length = 5) {
  */
 function broadcastMessage(data) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, data);
+        if (tabs.length > 0) {
+            chrome.tabs.sendMessage(tabs[0].id, data);
+        }
     });
     chrome.runtime.sendMessage(data);
 }
