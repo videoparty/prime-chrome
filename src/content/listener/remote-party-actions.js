fe79576e58f05c75bfd86501d3d14295cb4fe1f4
Marnix <dev@primevideoparty.com>
2020-05-02 12:30:24 +0200

    Showing notifications for our own actions

--- src/content/listener/remote-party-actions.js
+++ src/content/listener/remote-party-actions.js
@@ -3,43 +3,43 @@
  */
 
 listenToWindowEvent('start-video', async (ev) => {
-    if (!ev.data.remote) {return}
-    sendNotification('info', ev.data.byMemberName + ' started a video', 'Loading..');
+    const memberName = ev.data.remote ? ev.data.byMemberName : 'You';
+    sendNotification('info', memberName + ' started a video', 'Loading..');
 });
 
 listenToWindowEvent('pause-video', async (ev) => {
-    if (!ev.data.remote) {return}
-    sendNotification('info', ev.data.byMemberName + ' paused');
+    const memberName = ev.data.remote ? ev.data.byMemberName : 'You';
+    sendNotification('info', memberName + ' paused');
 });
 
 listenToWindowEvent('play-video', async (ev) => {
-    if (!ev.data.remote) {return}
-    if (ev.data.byMemberName) {
-        sendNotification('info', ev.data.byMemberName + ' resumed');
-    } else if (ev.data.coordinated) {
+    if (ev.data.coordinated) {
         sendNotification('info', 'Everyone is in sync', 'Resumed');
+    } else {
+        const memberName = ev.data.remote ? ev.data.byMemberName : 'You';
+        sendNotification('info', memberName + ' resumed');
     }
 });
 
 listenToWindowEvent('seek-video', async (ev) => {
-    if (!ev.data.remote) {return}
-    sendNotification('info', ev.data.byMemberName + ' seeked to another time');
+    const memberName = ev.data.remote ? ev.data.byMemberName : 'You';
+    sendNotification('info', memberName + ' seeked to another time');
 });
 
 listenToWindowEvent('close-video', async (ev) => {
-    if (!ev.data.remote) {return}
-    sendNotification('warning', ev.data.byMemberName + ' closed the video');
+    const memberName = ev.data.remote ? ev.data.byMemberName : 'You';
+    sendNotification('warning', memberName + ' closed the video');
 });
 
 listenToWindowEvent('member-change', async (ev) => {
-    if (!ev.data.remote) {return}
+    const memberName = ev.data.remote ? ev.data.member.displayName : 'You';
     if (ev.data.change === 'join') {
         if (ev.data.pause && player) {
-            sendNotification('success', ev.data.member.displayName + ' joined the party! Waiting for ' + ev.data.member.displayName + ' to sync up..');
+            sendNotification('success', memberName + ' joined the party! Waiting for ' + memberName + ' to sync up..');
         } else {
-            sendNotification('success', ev.data.member.displayName + ' joined the party!');
+            sendNotification('success', memberName + ' joined the party!');
         }
     } else if (ev.data.change === 'leave') {
-        sendNotification('error', ev.data.member.displayName + ' left the party');
+        sendNotification('error', memberName + ' left the party');
     }
 });
