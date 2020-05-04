ec636ceee5ca63fa64547d603563807a8dfb4dd9
Marnix <dev@primevideoparty.com>
2020-05-04 20:10:20 +0200

    Better error handling for play

--- src/content/actions/play.js
+++ src/content/actions/play.js
@@ -10,8 +10,12 @@ listenToWindowEvent('play-video', async () => {
             await player.play();
         } catch(err) {
             console.error(err);
-            onPause(); // Trigger the onPause to broadcast a message and pause all other members.
-            sendNotification('error', 'Could not play automatically. Please press the play button manually!', 'Error');
+            if (err.includes('https://goo.gl/LdLk22')) {
+                setTimeout(() => { player.play(); }, 50);
+            } else {
+                onPause(); // Trigger the onPause to broadcast a message and pause all other members.
+                sendNotification('error', 'Could not play automatically. Please press the play button manually!', 'Error');
+            }
         }
         player.onplay = onPlay;
     }
