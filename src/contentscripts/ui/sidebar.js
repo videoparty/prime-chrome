be50be52897f178a346ca98fd94e889652157dd0
Marnix <dev@primevideoparty.com>
2020-05-05 18:48:18 +0200

    Rename content to contentscripts

---
+++ src/contentscripts/ui/sidebar.js
@@ -0,0 +1,47 @@
+/**
+ * Inject the PvP sidebar in the page
+ * after the user joined the party
+ */
+let sidebarInitialized = false;
+listenToWindowEvent('member-change', (ev) => {
+    if (ev.data.change !== 'join' || sidebarInitialized) return;
+    const sidebarSrc = chrome.runtime.getURL('src/sidebar/sidebar.html');
+    jQuery('body').append('' +
+        '<div id="pvp-sidebar">' +
+        '<iframe id="pvp-sidebar-iframe" src="' + sidebarSrc + '"></iframe>' +
+        '</div>');
+    jQuery('#a-page').css('transition', 'width 1s ease-in-out').css('width', '100%');
+
+    // I like animations
+    setTimeout(() => {
+        jQuery('#pvp-sidebar').css('width', '20%');
+        jQuery('#a-page').css('width', '80%');
+    }, 1000);
+
+    startWebplayerWatcher();
+    sidebarInitialized = true;
+});
+
+/**
+ * Adjusts the PvP sidebar theme when the webplayer opens or closes.
+ */
+function startWebplayerWatcher() {
+    listenToWindowEvent('start-video', () => {
+        const sidebar = jQuery('#pvp-sidebar');
+        sidebar.addClass('player-mode');
+        adjustPlayerWidth(sidebar.css('width'));
+    });
+
+    listenToWindowEvent('close-video', () => {
+        const sidebar = jQuery('#pvp-sidebar');
+        sidebar.removeClass('player-mode');
+        adjustPlayerWidth(sidebar.css('width'));
+    });
+
+    function adjustPlayerWidth(sidebarWidth) {
+        const webPlayer = jQuery('#dv-web-player');
+        if (webPlayer.length > 0) {
+            webPlayer.css('width', 'calc(100% - ' + sidebarWidth + ')');
+        }
+    }
+}
