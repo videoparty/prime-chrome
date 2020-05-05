e75505b565b53897a322300c520850b6df4668dc
Marnix <dev@primevideoparty.com>
2020-05-05 14:16:49 +0200

    Removed dependency to primevideoparty.com assets

--- src/content/ui/navbar.js
+++ src/content/ui/navbar.js
@@ -6,11 +6,10 @@ jQuery(document).ready(() => {
     jQuery('.pv-nav-container').append('' +
         '<div id="pv-party" style="margin-left: 15px;display: flex;cursor: help;justify-content: center;width: 110px;" title="Amount of members in party">' +
         '<div style="width: 100%;height: 100%;">' +
-        '<img src="https://primevideoparty.com/pv-logo.png" style="height: 36px;" alt="Party" />' +
+        '<img src="'+chrome.runtime.getURL('icon-small.png')+'" style="height: 36px;" alt="Party" />' +
         '</div>' +
         '<div class="party-user-count" style="margin: 7px 0 0 5px;width: 100%;height: 100%;">' +
-        '<span id="party-member-count">' + memcount + '</span>' +
-        '<img src="https://primevideoparty.com/users.png" style="margin-top: 5px;" alt="users" />' +
+        '<span id="party-member-count">' + memcount + '</span> 🙋‍♂️' +
         '</div>' +
         '</div>');
 });
